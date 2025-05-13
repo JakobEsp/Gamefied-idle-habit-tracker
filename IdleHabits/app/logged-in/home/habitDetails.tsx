083e133953943d4b4dc29c9f3d910b0771d5f78d
JakobEsp <jakobespersen0@gmail.com>
2025-05-13 08:06:10 +0200

    resuable container styles

--- IdleHabits/app/logged-in/home/habitDetails.tsx
+++ IdleHabits/app/logged-in/home/habitDetails.tsx
@@ -1,9 +1,20 @@
+import { View } from "react-native";
+import CustomTextInput from "../../../components/TextInput";
+import Button from "../../../components/Button";
+import containers from "../../../styles/containers";
 
 
 export default function HabitDetails() {
 
     return (
-        <>
-        </>
+        <View
+            style={containers.container}
+        >
+            <CustomTextInput
+            
+            />
+            {/* Effort picker */}
+            <Button title="Create"></Button>
+        </View>
     )
 }
---
+++ IdleHabits/styles/containers.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const containers = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        gap: 10,
+    }
+})
+
+export default containers;  
