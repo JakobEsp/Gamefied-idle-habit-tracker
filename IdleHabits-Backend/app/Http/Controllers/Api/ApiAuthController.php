7370a2e19f7ab026f4f06bcbcf349083073fd6ed
JakobEsp <jakobespersen0@gmail.com>
2025-05-13 16:53:24 +0200

    login created

--- IdleHabits-Backend/app/Http/Controllers/Api/ApiAuthController.php
+++ IdleHabits-Backend/app/Http/Controllers/Api/ApiAuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class ApiAuthController extends Controller
 {
@@ -18,7 +19,25 @@ class ApiAuthController extends Controller
      */
     public function login(Request $request)
     {
-        
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
     }
 
     /**
